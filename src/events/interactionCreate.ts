1124d7e72a9089d0243e4f647fce0e14744056c1
Diego17 <diegoalbertocastropastor@gmail.com>
2026-01-19 22:08:45 -0500

    feat: enhance interaction handling and register settings command

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,10 +1,17 @@
 import { Events, Interaction } from "discord.js";
 import { interactionContextMiddleware } from "@/middlewares";
 import { handleLearnCommand } from "@/modules/learning/commands/learn.commands";
+import { handleSettingsCommand, handleSettingsHandlers, handleSettingsModal } from "@/modules/settings";
 
 export const name = Events.InteractionCreate;
 
 export const handleInteraction = async (interaction: Interaction) => {
+    if (interaction.isStringSelectMenu()) {
+        if (interaction.customId.startsWith("settings:")) return handleSettingsHandlers(interaction);
+    }
+    if (interaction.isModalSubmit()) {
+        if (interaction.customId.startsWith("settings:")) return handleSettingsModal(interaction);
+    }
     if (!interaction.isChatInputCommand()) return;
     
     await interactionContextMiddleware(interaction);
@@ -12,7 +19,11 @@ export const handleInteraction = async (interaction: Interaction) => {
         case "learn":
             await handleLearnCommand(interaction);
             break;
+        case "settings":
+            await handleSettingsCommand(interaction);
+            break;
         default:
             console.warn(`Unknown command: ${interaction.commandName}`);
     }
+    return;
 };
--- src/scripts/register-commands.ts
+++ src/scripts/register-commands.ts
@@ -1,5 +1,5 @@
 import { REST, Routes } from "discord.js";
-import { learnCommand } from "@/commands";
+import { learnCommand, settingsCommand } from "@/commands";
 import { env } from "@/config";
 
 const rest = new REST({ version: "10" }).setToken(env.DISCORD_TOKEN);
@@ -7,7 +7,7 @@ const rest = new REST({ version: "10" }).setToken(env.DISCORD_TOKEN);
 export async function registerCommands() {
     await rest.put(
         Routes.applicationCommands(env.APP_ID),
-        { body: [learnCommand.toJSON()] },
+        { body: [learnCommand.toJSON(), settingsCommand.toJSON()] },
     )
     console.log("Commands registered.");
 }
